dd29c36a6d2004c67bb722c92cdf9374e7254a6b
Raymond Moody <144750662+Raymond-Moody@users.noreply.github.com>
2025-12-15 17:31:06 -0500

    Day 4 part 2

--- day4/solution.c
+++ day4/solution.c
@@ -37,8 +37,10 @@ int accessibleRolls(char *filename) {
 	int grid_size = 0;
 	generateLayout(filename, &grid, &grid_size);
 
-	if (grid == NULL || grid_size == 0)
+	if (grid == NULL || grid_size == 0) {
+		puts("Failed to generate layout from input.");
 		return 0;
+	}
 
 	for (int i = 0; i < grid_size * grid_size; ++i) {
 		int neighbors = 0;
@@ -68,13 +70,61 @@ int accessibleRolls(char *filename) {
 	return result;
 }
 
+int accessibleWithRemoval(char *filename) {
+	int result = 0;
+	char *grid = NULL;
+	int grid_size = 0;
+	int removed = 1;
+	generateLayout(filename, &grid, &grid_size);
+
+	if (grid == NULL || grid_size == 0) {
+		puts("Failed to generate layout from input.");
+		return 0;
+	}
+
+	while (removed) {
+		removed = 0;
+		for (int i = 0; i < grid_size * grid_size; ++i) {
+			int neighbors = 0;
+			if (grid[i] == '.')
+				continue;
+		
+			for (int dy = -1; dy <= 1; ++dy) {
+				int row = i / grid_size + dy;
+				if (row < 0 || row >= grid_size)
+					continue;
+			
+				for (int dx = -1; dx <= 1; ++dx) {
+					int col = i % grid_size + dx;
+					if (col < 0 || col >= grid_size)
+						continue;
+					if (dx == 0 && dy == 0)
+						continue;
+					if (grid[row * grid_size + col] == '@')
+						neighbors++;
+				}
+			}
+			if (neighbors < 4) {
+				grid[i] = '.';
+				removed = 1;
+				result++;
+			}
+		}
+	}
+	
+	free(grid);
+	return result;
+}
+
 int main(int argc, char **argv) {
 	if (argc < 2) {
 		puts("Please provide a filename");
 		return 1;
 	}
 	
-	int rolls = accessibleRolls(argv[1]);
-	printf("%d rolls accessible.\n", rolls);
+	int p1 = accessibleRolls(argv[1]);
+	int p2 = accessibleWithRemoval(argv[1]);
+	printf("%d rolls accessible.\n", p1);
+	printf("%d rolls accessible with removal.\n", p2);
 	return 0;
 }
